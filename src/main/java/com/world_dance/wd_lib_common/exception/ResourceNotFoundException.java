82cac19c90f2175b786fc3c20dfb3c5edfd2ed42
JuanDiego110508 <juandiegolr28@gmail.com>
2026-09-03 20:23:30 -0500

    refactor: Se agregaron excepciones de scheduling

---
+++ src/main/java/com/world_dance/wd_lib_common/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.world_dance.wd_lib_common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
